6eb019bec5876a15c19634d0cf1c7bc8c6f1fdb0
Paresh-ch-Pothal <pareshchandrapothal44@gmail.com>
2026-03-23 23:17:58 +0530

    Vercel.json and other requirements has been added

--- python-server/src/main.py
+++ python-server/src/main.py
@@ -1,5 +1,23 @@
+# # src/main.py
+# from src import create_app,db
+# from src.database.models import Bus
+# from src.redis.redisSubscribe import subscribe_to_redis
+
+# app = create_app()
+
+# # For dev only: create tables automatically
+# with app.app_context():
+#     db.create_all()
+
+# if __name__ == "__main__":
+#     subscribe_to_redis()
+#     app.run(debug=True, host="0.0.0.0", port=5000)
+
+
 # src/main.py
-from src import create_app,db
+import os
+import threading
+from src import create_app, db
 from src.database.models import Bus
 from src.redis.redisSubscribe import subscribe_to_redis
 
@@ -9,6 +27,14 @@ app = create_app()
 with app.app_context():
     db.create_all()
 
-if __name__ == "__main__":
+# Run Redis subscriber in a separate thread
+def start_redis_listener():
     subscribe_to_redis()
-    app.run(debug=True, host="0.0.0.0", port=5000)
+
+t = threading.Thread(target=start_redis_listener)
+t.daemon = True  # ensures thread exits when main process exits
+t.start()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render assigns PORT
+    app.run(debug=False, host="0.0.0.0", port=port)
